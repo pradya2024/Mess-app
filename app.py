c4302c2a0e8e9fd71d342da04cedded9783361f9
pradya2024 <bsk821993@gmail.com>
2026-09-08 08:09:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # 🟢 APNI GOOGLE WEB APP KI LINK YAHAN PASTE KAREIN
-SCRIPT_URL = "https://google.com"
+SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxbbUBdxj__qToZfF33nT2E3E464K9i3v6S9vDSaxLx4ll8nwNrY8gDaDQqN2sfJbQ2/exec"
 
 # Shop Main Banner on Top
 st.markdown('<div class="shop-header"><h1>🚩 ANNAPURNA VEGETABLE SHOP</h1><p>Mess Supply Demand & Billing System</p></div>', unsafe_allow_html=True)
